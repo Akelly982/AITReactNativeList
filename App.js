cccde573ce1d9e7e38a6baf46f17b8a1a06a7482
Akelly982 <Akelly982@users.noreply.github.com>
2021-06-23 11:58:42 +1000

    update adding rnPicker

--- App.js
+++ App.js
@@ -4,8 +4,15 @@ import {useState,useEffect} from 'react';
 import { StyleSheet, Text, View, FlatList, TouchableOpacity, TextInput } from 'react-native';
 
 
+//import from another file
+import {defaultListItem} from "./components/defaultListItem";
+import {DropDown} from "./components/DropDown"
 
-catagories = [
+
+
+
+// Array
+let catagories = [
   { label: 'Food', value: "food"},
   { label: 'Coffee', value: 'coffee'},
   { label: 'Transport', value: 'transport'},
@@ -18,30 +25,53 @@ catagories = [
 
 
 export default function App() {
+
+  //states
+  const [category, setCategory] = useState()
+  const [ammout, setAmmount] = useState()
+
+
+  const dropdownChange = (value) => {
+    //console.log(value)
+    setCategory(value)
+  }
+  
+  const ammountChange = (value) => {
+    console.log(value)
+  }
+
   return (
     <View style={styles.margin}>
       <View style={styles.container}>
         <Text>Money Tracker</Text>
         <View>
-          <TextInput />
+          <TextInput style={styles.input} onChangeText={ammountChange} keyboardType={'number-pad'}/>
+          <DropDown catagories={catagories} handler={dropdownChange} />
         </View>
       </View>
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
   margin:{
     marginVertical: 20,
     marginHorizontal: 20,
-    backgroundColor: "red",
   },
 
   container: {
     flex: 1,
     backgroundColor: '#fff',
     paddingTop: 30,
-    backgroundColor: "blue",
     
   },
+
+  input: {
+    backgroundColor: "#cccccc",
+    padding: 10,
+    fontSize: 20,
+  },
+
+
 });
